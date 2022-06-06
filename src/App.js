a855064058cab3ea546242fa8e339ddad9585347
unknown <muhammadismailsoomro09@gmail.com>
2022-06-05 21:47:12 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import './App.css';
+import { BrowserRouter as Router, Routes , Route} from 'react-router-dom';
+import Home from './screens/homeScreen/Home';
+import Navbar from './components/navbar/Navbar';
+import About from './screens/aboutScreen/About';
+import Login from './screens/loginScreen/Login';
+import SignUp from './screens/signUpScreen/SignUp';
+import Test from './screens/testScreen/Test';
+import Test from './screens/notFoundScreen/NotFound';
+import Footer from './components/footer/Footer';
+
+
+function App() {
+  return (
+  <>
+    <Router>
+      <Navbar />
+      <Routes >
+        <Route path='/' exact element={<Home/>} />
+        <Route path='/about'  element={<About/>} />
+        <Route path='/login'  element = {<Login />} />
+        <Route path='/sign-up'  element = {<SignUp />} />
+        <Route path='/test' element = {<Test />} />
+        <Route path='*' element = {<NotFound />} />
+      </Routes >
+    </Router>
+    <Footer />
+
+  </>
+  );
+}
+
+export default App;
